9458938f5ec94ad0c319ac8ab6efcb9fc22fc4fc
Johannes <johannes.knaebel@campus.tu-berlin.de>
2023-11-18 18:48:16 +0100

    added median filter

--- Dip2.cpp
+++ Dip2.cpp
@@ -8,6 +8,9 @@
 
 #include "Dip2.h"
 
+using namespace std;
+using namespace cv;
+
 namespace dip2
 {
 
@@ -85,12 +88,46 @@ namespace dip2
      * @param kSize Window size used by median operation
      * @returns Filtered image
      */
-    cv::Mat_<float> medianFilter(const cv::Mat_<float> &src, int kSize)
+    cv::Mat_<float> medianFilter(const cv::Mat_<float>& src, int kSize)
     {
-        // TO DO !!
-        cv::Mat kernel = cv::Mat::ones(kSize, kSize, CV_32FC1) / (float)(kSize * kSize); // Create a kernel with all elements equal to 1/(kSize*kSize)
-        // std::cout << kernel << std::endl;
-        return spatialConvolution(src, kernel);
+
+        int km = kSize+1/2; // kernel middle
+        int border = km-1;
+
+        Mat src_gray = src;
+
+        Mat src_wb(src.rows + border*2, src.cols + border*2, src.depth());
+
+        copyMakeBorder(src_gray, src_wb, border, border, border, border, BORDER_REPLICATE);
+
+        Mat output = src;
+
+        for (int xp = 0; xp <= src.rows-1; xp++)
+        {
+            for (int yp= 0; yp <= src.cols-1; yp++)
+            {
+                vector<float> values;
+
+                for (int xk = 0; xk <= kSize-1; xk++)
+                {
+                    for (int yk=0; yk <= kSize-1; yk++)
+                    {
+
+                        values.push_back(src_wb.at<float>(xp+xk,yp+yk));
+
+                    }
+                }
+                std::sort(values.begin(), values.end());
+                // cout << " MedFilter value = " << values[(((kSize*kSize)-1)/2)]  << endl; // is 1 ?
+                output.at<float>(xp,yp) = values[(((kSize*kSize)-1)/2)];
+
+            }
+        }
+
+
+        //  cout << " MedFilter kSize= " << kSize  << endl; // is 1 ?
+        imwrite("test_med.jpg", output); // why is pic so dark?
+        return output;
     }
 
     /**
@@ -143,9 +180,9 @@ namespace dip2
             switch (noiseType)
             {
             case NOISE_TYPE_1:
-                return dip2::averageFilter(src, 33);
+                return dip2::averageFilter(src, 3);
             case NOISE_TYPE_2:
-                return dip2::averageFilter(src, 33);
+                return dip2::averageFilter(src, 3);
             default:
                 throw std::runtime_error("Unhandled noise type!");
             }
@@ -153,9 +190,9 @@ namespace dip2
             switch (noiseType)
             {
             case NOISE_TYPE_1:
-                return dip2::medianFilter(src, 33);
+                return dip2::medianFilter(src, 3);
             case NOISE_TYPE_2:
-                return dip2::medianFilter(src, 33);
+                return dip2::medianFilter(src, 3);
             default:
                 throw std::runtime_error("Unhandled noise type!");
             }
